bf9229cfa446b3db7988b80c0f3e266116038087
Naman <mymail.naman@gmail.com>
2022-06-14 02:40:41 +0530

    Added proper list

--- src/index.js
+++ src/index.js
@@ -1,62 +1,53 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 // CSS
 import  './index.css';
+const books = [
+  { 
+    id: "1",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg",
+    name: "The Psychology of Money",
+    author: "Morgan Housel",
+    prop: "Paperback",
+    price: "Rs. 259.00"
+  },
+  {
+    id: "2",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/71sBtM3Yi5L._AC_UL604_SR604,400_.jpg",
+    name: "The power of your subconscious Mind",
+    author: "Joseph Murphy",
+    prop: "Paperback",
+    price: "Rs. 115.00"
+  },
+  {
+    id: "3",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/61boBtfrz5L._AC_UL604_SR604,400_.jpg",
+    name: "Ret Samadhi - Hindi",
+    author: "Geetanjali Shree",
+    prop: "Paperback",
+    price: "Rs. 312.00"
+  }
+];
 
-const bookOne = {
-  img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL604_SR604,400_.jpg",
-  name: "The Psychology of Money",
-  author: "Morgan Housel",
-  prop: "Paperback",
-  price: "Rs. 259.00"
-}
-const bookTwo = {
-  img: "https://images-eu.ssl-images-amazon.com/images/I/71sBtM3Yi5L._AC_UL604_SR604,400_.jpg",
-  name: "The power of your subconscious Mind",
-  author: "Joseph Murphy",
-  prop: "Paperback",
-  price: "Rs. 115.00"
-}
-const bookThree = {
-  img: "https://images-eu.ssl-images-amazon.com/images/I/61boBtfrz5L._AC_UL604_SR604,400_.jpg",
-  name: "Ret Samadhi - Hindi",
-  author: "Geetanjali Shree",
-  prop: "Paperback",
-  price: "Rs. 312.00"
-}
 
-
-function Books() {
+function BookList() {
   return (
     <section className='bookList'>
-      <Book 
-      img = {bookOne.img}
-      name={bookOne.name}
-      author={bookOne.author}
-      prop={bookOne.author}
-      price={bookOne.price}/>
-      <Book
-        img={bookTwo.img}
-        name={bookTwo.name}
-        author={bookTwo.author}
-        prop={bookTwo.author}
-        price={bookTwo.price} />
-      <Book 
-        img={bookThree.img}
-        name={bookThree.name}
-        author={bookThree.author}
-        prop={bookThree.author}
-        price={bookThree.price} />
+      {books.map((book) => {
+        const { img, name, author, prop, price } = book
+         return <Book key = {book.id} {...book}></Book>
+      })}
     </section>
   )
 }
 
-const Book = ({img,name,author,prop,price}) => {
+const Book = (props) => {
+  const { img, name, author, prop, price } = props;
   return (
     <article className='book'>
       <img src={img} alt="" />
-      <h2>{name}</h2>
+      <p className='title'>{name}</p>
       <p className='author'>{author}</p>
       <p className='prop'>{prop}</p>
       <h6>{price}</h6>
@@ -64,4 +55,9 @@ const Book = ({img,name,author,prop,price}) => {
   )
 } 
 
-ReactDom.render(<Books />, document.getElementById('root'));
+// ReactDom.render(< />, document.getElementById('root'));
+
+
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(<BookList tab="home" />);
